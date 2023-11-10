9b3e8a123f3dd17a2d8a0736a88e85f19c26daac
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2023-11-10 21:27:09 +0000

    ✅ Pass tests

--- mimesis_and_ge/transformations.py
+++ mimesis_and_ge/transformations.py
@@ -1,6 +1,20 @@
+import pandas as pd
+from mimesis import Fieldset
+from mimesis.locales import Locale
+
+fs = Fieldset(locale=Locale.EN, i=5)
+
+
 def add_offset(augend: int, addend: int) -> int:
     return augend + addend
 
 
 def make_dataframe():
-    pass
+    return pd.DataFrame.from_dict(
+        {
+            "ID": fs("increment"),
+            "Name": fs("person.full_name"),
+            "Email": fs("email"),
+            "Phone": fs("telephone", mask="+1 (###) #5#-7#9#"),
+        }
+    )
